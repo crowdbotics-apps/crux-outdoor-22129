2970a85f1047c0ac0b35cfceeaa4118ac5c9fdd9
Crowdbotics <team@crowdbotics.com>
2020-10-30 17:55:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/crux_outdoor_22129/MainActivity.java
@@ -0,0 +1,15 @@
+package com.crux_outdoor_22129;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "crux_outdoor_22129";
+    }
+}
